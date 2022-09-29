6600b1ee4e1fd3baf3eceb78f6aca686cd7402be
Juan Pablo Alvarado <alvaradovallejo.juan@gmail.com>
2022-09-28 20:09:55 -0500

    PCA and IPCA Implementation

--- pca.py
+++ pca.py
@@ -18,4 +18,28 @@ if __name__ == '__main__':
 
     dt_features = StandardScaler().fit_transform(dt_features)
     
-    X_train, X_test, X_train, Y_train = train_test_split(dt_features, dt_target, test_size=0.3, random_state=42)
+    X_train, X_test, Y_train, Y_test = train_test_split(dt_features, dt_target, test_size=0.3, random_state=42)
+
+    print(X_train.shape)
+    print(Y_train.shape)
+
+    pca = PCA(n_components=3)
+    pca.fit(X_train)
+
+    ipca = IncrementalPCA(n_components=3, batch_size=10)
+    ipca.fit(X_train)
+
+    plt.plot(range(len(pca.explained_variance_)), pca.explained_variance_ratio_)
+    plt.show()
+
+    logistic = LogisticRegression(solver='lbfgs')
+
+    dt_train = pca.transform(X_train)
+    dt_test = pca.transform(X_test)
+    logistic.fit(dt_train, Y_train)
+    print("SCORE PCA: ", logistic.score(dt_test, Y_test))
+
+    dt_train = ipca.transform(X_train)
+    dt_test = ipca.transform(X_test)
+    logistic.fit(dt_train, Y_train)
+    print("SCORE IPCA: ", logistic.score(dt_test, Y_test))
